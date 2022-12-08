bb451ea17de6aaf4852380e680d7a32fcfbb3e66
dokuyucufirat <dokuyucufirat@gmail.com>
2022-12-08 19:31:05 +0300

    firsttThingsFirst

---
+++ Labels/Labels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Labels
+//
+//  Created by Fırat Dokuyucu on 8.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
